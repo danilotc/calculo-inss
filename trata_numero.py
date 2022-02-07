ad18860567c9ab3424af9085208de0457f813853
Danilo Castro <danilotrindade87@gmail.com>
2022-02-07 00:49:46 +0000

    Deixando a função mais clara.

--- trata_numero.py
+++ trata_numero.py
@@ -1,8 +1,8 @@
 def trunca_decimal(valor, decimal):
 	"""
-    Recebe um valor real e a quantidade de casas decimais desejada,
-    retorna o mesmo valor com 2 casas decimais não arredondadas.
-    """
+	Recebe um valor real e a quantidade de casas decimais desejada,
+	retorna o mesmo valor com 2 casas decimais não arredondadas.
+	"""
 	lista = str(valor).split('.')
 	inteiro = lista[0]
 	decimal = lista[1][:decimal]
